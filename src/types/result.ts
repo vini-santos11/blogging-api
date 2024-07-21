c3b1851405bd343819590de6501e048e7ccc3805
beatriz-penalva <biapenalva@gmail.com>
2024-07-21 11:46:34 -0300

    refactor: add null as default for data param on result constructor

--- src/types/result.ts
+++ src/types/result.ts
@@ -3,7 +3,7 @@ export class Result<T> {
     message: string | null;
     data: T;
 
-    constructor(statusCode: number, message: string | null, data: T) {
+    constructor(statusCode: number, message: string | null, data: T = null) {
         this.statusCode = statusCode;
         this.message = message;
         this.data = data;
