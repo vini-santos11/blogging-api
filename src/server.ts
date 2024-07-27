5de5ad653a6d9e8cfe21d0656fbafe931356b91a
luscaaaa <l.paraguai33@gmail.com>
2024-07-27 01:40:51 -0300

    refactor: added a catch when initializing the database and env port

--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,22 @@ import { AppDataSource } from './database/data-source';
 import routes from "./routes/index.routes";
 import cors from 'cors';
 import "reflect-metadata";
+import 'dotenv/config'
+import { validateJwt } from "./middlewares/validate-jwt";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(validateJwt)
 app.use(routes);
 
 AppDataSource.initialize().then(() => {
     console.log('Database connected');
+    const PORT = process.env.PORT || 3001
 
-    app.listen(3001, async () => {
-        console.log('Server is running on port 3001');
+    app.listen(PORT, async () => {
+        console.log(`Server is running on port ${PORT}`);
     })
+}).catch((e) => {
+    console.log("Cannot connect to database server:", e)
 })
